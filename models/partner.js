ab35c27c76f2a9eb463af4ccfc2cae8b44b421c7
Adia Morris Swanger <adiamorrisswanger@gmail.com>
2022-03-05 11:31:45 -0600

    Week 2 Workshop Assignment

---
+++ models/partner.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema({
+    name: {
+       type: String,
+       required: true,
+       unique: true 
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    description: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true
+});
+
+const Partner = mongoose.model('Parter', partnerSchema);
+
+module.exports = Partner;
